daa705c2895e587bb42681952e05372d89b0702e
Wang <mac@mac-2.local>
2019-04-06 19:54:31 +0800

    update

--- AI.Learning/AI.Learning.Notes.DeepLearningFlappyBird/deep_q_network.py
+++ AI.Learning/AI.Learning.Notes.DeepLearningFlappyBird/deep_q_network.py
@@ -11,13 +11,14 @@ import numpy as np
 from collections import deque
 
 GAME = 'bird' # the name of the game being played for log files
-ACTIONS = 2 # number of valid actions
+ACTIONS = 2 # action数目
 GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 100000. # timesteps to observe before training
+OBSERVE = 100000. # 在训练之前的预热期
 EXPLORE = 2000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.0001 # starting value of epsilon
-REPLAY_MEMORY = 50000 # number of previous transitions to remember
+# e-greedy策略的初始和最终值
+FINAL_EPSILON = 0.0001
+INITIAL_EPSILON = 0.0001
+REPLAY_MEMORY = 50000 # 状态存储的大小
 BATCH = 32 # size of minibatch
 FRAME_PER_ACTION = 1
 
@@ -52,7 +53,7 @@ def createNetwork():
     W_fc2 = weight_variable([512, ACTIONS])
     b_fc2 = bias_variable([ACTIONS])
 
-    # input layer
+    # 输入层，80x80的图片，4帧
     s = tf.placeholder("float", [None, 80, 80, 4])
 
     # hidden layers
@@ -72,14 +73,17 @@ def createNetwork():
 
     # readout layer
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
-
+    # 返回输入层，action长度的输出向量，以及512长度，降维成2长度向量前的向量
     return s, readout, h_fc1
 
 def trainNetwork(s, readout, h_fc1, sess):
-    # define the cost function
+    # 输入的action，以及输出的值的placeholder
     a = tf.placeholder("float", [None, ACTIONS])
     y = tf.placeholder("float", [None])
+    # 输入action，得到指定action的value，这个value要和得到的reward尝试去匹配，减去loss
+    # NOTICE：输入S得到的是向量，但是此外还需要输入action，得到reward
     readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
+    # 将action得到value的向量和y求取loss
     cost = tf.reduce_mean(tf.square(y - readout_action))
     train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
@@ -96,9 +100,11 @@ def trainNetwork(s, readout, h_fc1, sess):
     # get the first state by doing nothing and preprocess the image to 80x80x4
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
+    # 输入操作，得到所有观测状态（像素）
     x_t, r_0, terminal = game_state.frame_step(do_nothing)
     x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
     ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
+    # 将初始的同一个像素stack成4个，保持初始输入的shape
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
 
     # saving and loading networks
@@ -114,11 +120,13 @@ def trainNetwork(s, readout, h_fc1, sess):
     # start training
     epsilon = INITIAL_EPSILON
     t = 0
-    while "flappy bird" != "angry bird":
+    while 1:
         # choose an action epsilon greedily
+        # 输入当前的状态
         readout_t = readout.eval(feed_dict={s : [s_t]})[0]
         a_t = np.zeros([ACTIONS])
         action_index = 0
+        # 如果小于epsilon，则随机选取操作，否则选取Q值更大的操作
         if t % FRAME_PER_ACTION == 0:
             if random.random() <= epsilon:
                 print("----------Random Action----------")
@@ -130,11 +138,13 @@ def trainNetwork(s, readout, h_fc1, sess):
         else:
             a_t[0] = 1 # do nothing
 
-        # scale down epsilon
+        # epsilon越大，随机性越强
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
+        # 采取相应的操作，然后得到新的状态
         # run the selected action and observe next state and reward
+        # 这里会给出reward！reward是游戏给出的！一般状态正常为0.1，死亡为1，穿越管子为1啥的
         x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
         x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
@@ -143,15 +153,18 @@ def trainNetwork(s, readout, h_fc1, sess):
         s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
         # store the transition in D
+        # D，非常重要，存储上一状态，action，reward，下一状态，以及游戏是否结束
         D.append((s_t, a_t, r_t, s_t1, terminal))
         if len(D) > REPLAY_MEMORY:
             D.popleft()
 
         # only train if done observing
+        # 在完成观察期之后开始训练
         if t > OBSERVE:
             # sample a minibatch to train on
             minibatch = random.sample(D, BATCH)
-
+            # 从存储的D状态中随机选取batch个样本，这些样本的行动一些是随机的，一些是DQN决策的，总之不管
+            # 全部拿出来作为训练
             # get the batch variables
             s_j_batch = [d[0] for d in minibatch]
             a_batch = [d[1] for d in minibatch]
@@ -159,16 +172,21 @@ def trainNetwork(s, readout, h_fc1, sess):
             s_j1_batch = [d[3] for d in minibatch]
 
             y_batch = []
+            # 注意这里输出的是t+1的reward
             readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
                 # if terminal, only equals reward
+                # 如果游戏结束了，那么reward就设置为游戏给出的reward
                 if terminal:
                     y_batch.append(r_batch[i])
+                # 如果游戏没有结束，reward就是DQN预测出的reward的最大值乘以gamma，加上t+1给出的reward
+
                 else:
                     y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
-
+            # 使用y作为误差重新训练使得预测更为精确
             # perform gradient step
+            #NOTICE： 这里很重要！当前的reward是t时刻的reward加上GAMMA * t+1时刻的reward，这样就使用了下一帧的reward信息
             train_step.run(feed_dict = {
                 y : y_batch,
                 a : a_batch,
